87c75e31f2508d86f59e72717af7a921589ba990
iyeoleum <yl9517@naver.com>
2025-03-31 11:31:15 +0900

    Bean 생명주기
    Tomcat, dispatcher도 Bean으로 만들어서 스프링 컨테이너가 관리하게 변경

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,9 @@
 package tobyspring.helloboot;
 
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +15,14 @@ import java.util.Objects;
 @RestController //이 어노테이션은 반환되는 문자열이 알아서 응답body에 들어감 (없으면 view를 찾으려고 함)
 //@RequestMapping("/hello")
 //@MyComponent
-public class HelloController {
+public class HelloController{
   private final HelloService helloService;
+  private final ApplicationContext applicationContext;
 
   //application에서 등록된 bean 뒤져서 service 알아서 찾아냄.
-  public HelloController(HelloService helloService) {
+  public HelloController(HelloService helloService, ApplicationContext applicationContext) {
     this.helloService = helloService;
+    this.applicationContext = applicationContext;
   }
 
     @GetMapping("/hello")
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,35 +1,30 @@
 package tobyspring.helloboot;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import java.io.IOException;
 
 //@SpringBootApplication
 @Configuration //구성정보를 가지고 있는 클래스다! (-> 아 이 안에 bean이 붙은 factory 메소드가 있겠구나, 그걸 이용해서 bean객체 만들면 되겠다)
 @ComponentScan //component 붙은애들 찾아서 bean등록
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory serverFactory () {
+		return new TomcatServletWebServerFactory();
+	}
 
+	@Bean
+	public DispatcherServlet dispatcherServlet () {
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
 		//spring container 만들기
@@ -38,11 +33,12 @@ public class HellobootApplication {
 			protected void onRefresh() {
 				super.onRefresh();
 				//servlet container
-				TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				// dispatcherServlet.setApplicationContext(this); 없어도 됨. 스프링 컨테이너가 알아서 넣어줌
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-									new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet",dispatcherServlet).addMapping("/*");
 				});
 				webServer.start();
 			}
