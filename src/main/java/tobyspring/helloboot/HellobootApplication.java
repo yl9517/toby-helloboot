72167eb4a650bac8d8ce7b046597f9234bb0664c
iyeoleum <yl9517@naver.com>
2025-03-31 10:55:15 +0900

    Factory Method 사용해서 (자바코드로 만든) Bean 생성
    단순 적용테스트임. 굳이 이렇게 할 필요 없음
    
    목표 : 우리가 만든 자바코드를 어떻게 Object로 만들고, 어떻게 container 내에 component로 등록하지? Bean에 의존하고 있다면 어떻게 관계를 맺어주지? 어느 시점에 Object를 주입하지? 를 구성정보로 제공해 줄 것.
    
    @Configuration : 구성정보를 가지고 있는 클래스
    - 이 안에 bean을 가진 Factory Method가 있겠구나, 그걸 이용해서 Bean Object를 만들면 되겠다

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -11,22 +11,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.IOException;
 
 //@SpringBootApplication
+@Configuration //구성정보를 가지고 있는 클래스다! (-> 아 이 안에 bean이 붙은 factory 메소드가 있겠구나, 그걸 이용해서 bean객체 만들면 되겠다)
 public class HellobootApplication {
+
+	@Bean
+	public HelloController helloController(HelloService service){
+		return new HelloController(service);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		//spring container 만들기
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+			AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -41,8 +55,7 @@ public class HellobootApplication {
 			}
 		};
 
-		applicationContext.registerBean(HelloController.class); // Bean 등록
-		applicationContext.registerBean(SimpleHelloService.class); //Bean 등록
+		applicationContext.register(HellobootApplication.class);
 		applicationContext.refresh(); //초기화
 
 
